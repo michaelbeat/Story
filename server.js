ef508b6ed6e685e715ffe9b7e86b959e01560768
michaelBeat <michael.c.beat@gmail.com>
2019-01-07 21:26:24 -0600

    trying to get the articles to display

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/scrapednews", { useNewUrlParser: true });
 
 
 app.get("/scrape", function(req, res) {
